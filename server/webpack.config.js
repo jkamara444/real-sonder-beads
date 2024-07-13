4a78dd6f4a190f6b729192943d84d353cc850153
jkamara444 <jasmine_kamara@brown.edu>
2024-07-12 20:34:54 -0700

    ..

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: '/server/public/js/index.js', // Adjust the entry point to match your existing structure
+    entry: '/public/js/index.js', // Adjust the entry point to match your existing structure
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'server') // Adjust the output path as needed
